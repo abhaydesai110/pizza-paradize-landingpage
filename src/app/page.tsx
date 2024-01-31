f19b0d691fa7e2ae16c9d5a09303f778d4edd12b
agriwyzresearch <abhaydesai110@gmail.com>
2024-01-31 23:59:27 +0530

    project-setup-done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="w-screen">
+      <section className="max-w-[1440px] mx-auto bg-red-200">asd</section>
+    </main>
+  );
+};
+
+export default page;
