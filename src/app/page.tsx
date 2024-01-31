c6f6667273eda4ef7045d531e4f15aed525e50e9
agriwyzresearch <abhaydesai110@gmail.com>
2024-02-01 00:16:25 +0530

    setbghome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import home from "../assets/HomeBg.png";
 
 const page = () => {
   return (
-    <main className="w-screen">
-      <section className="max-w-[1440px] mx-auto bg-red-200">asd</section>
+    <main className="">
+      <section className="max-w-[1460px] h-[821px]  mx-auto bg-[url(../assets/HomeBg.png)]">
+        <nav className="max-w-[1208px] mx-auto px-2 flex items-center justify-between text-white">
+          <h1 className="text-2xl uppercase">LOGO</h1>
+          <div className="flex items-center justify-center space-x-9">
+            <p className="">Home</p>
+            <p className="">Menu</p>
+            <p className="">Order</p>
+            <p className="">Location</p>
+            <p className="">Reviews</p>
+          </div>
+          <button className="w-64 h-12 rounded-2xl border border-white">contact</button>
+        </nav>
+      </section>
     </main>
   );
 };
