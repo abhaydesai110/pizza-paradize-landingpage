b1eb8b3f66e039e1fff6b73a6578a1bd2f944145
Abhay Desai <abhaydesai110@gmail.com>
2024-02-02 01:10:19 +0530

    3rd page almost ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 // import home from "../assets/HomeBg.png";
 import Pizaa from "../assets/Pizaa.png";
 import PizaaLogo from "../assets/Vector.png";
+import chezzy7 from "../assets/7cheez.png";
 
 const page = () => {
   return (
@@ -32,9 +33,9 @@ const page = () => {
       </section>
       <section className="max-w-[1460px] mx-auto relative">
         <div className="w-full my-[100px]">
-          <div className="max-w-[1089px] my-14 flex items-center justify-center flex-wrap px-2 mx-auto">
-            <div className="max-w-full md:w-1/2 lg:w-1/3 flex flex-col items-center">
-              <img src={PizaaLogo.src} className="pb-[18px]" alt="" />
+          <div className="max-w-[1089px] my-14 flex  justify-center flex-wrap px-2 mx-auto">
+            <div className="max-w-full md:w-1/2 lg:w-1/3 flex flex-col  space-y-[18px] items-center">
+              <img src={PizaaLogo.src} className="" alt="" />
               <h1 className="text-2xl">ORIGINAL RECIPES</h1>
               <p className="text-center">
                 Immerse yourself in a world of innovative and unique pizzas
@@ -45,10 +46,10 @@ const page = () => {
               <img src={PizaaLogo.src} className="" alt="" />
               <h1 className="text-2xl">QUALITY FOOD</h1>
               <p className="text-center">
-              Our food is carefully prepared with the highest quality products to satisfy the pleasures of all appetites.
+                Our food is carefully prepared with the highest quality products to satisfy the pleasures of all appetites.
               </p>
             </div>
-            <div className="max-w-full md:w-1/2 lg:w-1/3 flex flex-col items-center">
+            <div className="max-w-full md:w-1/2 lg:w-1/3 flex  space-y-[18px] flex-col items-center">
               <img src={PizaaLogo.src} className="" alt="" />
               <h1 className="text-2xl">FAST DELIVERY</h1>
               <p className="text-center">
@@ -58,6 +59,57 @@ const page = () => {
             </div>
           </div>
         </div>
+      </section>
+      <section className="w-full">
+        <div className="max-w-[1208px] mx-auto px-2 ">
+          <h1 className="uppercase pb-[48px]">our menu</h1>
+          <div className="w-full flex items-center justify-center flex-wrap">
+            <div className="max-w-full pr-4  md:1/2 lg:1/3">
+              <div className="border rounded-b-2xl ">
+                <img src={chezzy7.src} className="w-full h-[289px]" alt="" />
+                <div className="p-[18px] flex flex-col ">
+                  <h2 className="text-[18px]">PEPPERONI PIZZA</h2>
+                  <p>Sliced pepperoni, parmesan cheese, mozzarella cheese, sauce</p>
+                  <div className="w-full flex items-center justify-between pt-[18px]">
+                    <button className="w-[93px] h-10 rounded-full border border-[#E29E00]">$22.00</button>
+                    <button className="bg-[#E29E00] w-[118px] h-10 rounded-full text-white">call</button>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="max-w-full px-4 md:1/2 lg:1/3">
+              <div className="border rounded-b-2xl ">
+                <img src={chezzy7.src} className="w-full h-[289px]" alt="" />
+                <div className="p-[18px] flex flex-col ">
+                  <h2 className="text-[18px]">PEPPERONI PIZZA</h2>
+                  <p>Sliced pepperoni, parmesan cheese, mozzarella cheese, sauce</p>
+                  <div className="w-full flex items-center justify-between pt-[18px]">
+                    <button className="w-[93px] h-10 rounded-full border border-[#E29E00]">$22.00</button>
+                    <button className="bg-[#E29E00] w-[118px] h-10 rounded-full text-white">call</button>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="max-w-full pl-4  md:1/2 lg:1/3">
+              <div className="border rounded-b-2xl ">
+                <img src={chezzy7.src} className="w-full h-[289px]" alt="" />
+                <div className="p-[18px] flex flex-col ">
+                  <h2 className="text-[18px]">PEPPERONI PIZZA</h2>
+                  <p>Sliced pepperoni, parmesan cheese, mozzarella cheese, sauce</p>
+                  <div className="w-full flex items-center justify-between pt-[18px]">
+                    <button className="w-[93px] h-10 rounded-full border border-[#E29E00]">$22.00</button>
+                    <button className="bg-[#E29E00] w-[118px] h-10 rounded-full text-white">call</button>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
       </section>
     </main>
   );
